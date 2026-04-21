c93db089d690ab49948184f7efba29dcd4e79409
JordiZ1234 <zalkakianiparnidzejordi@fpllefia.com>
2026-04-21 12:42:30 +0200

    asdads

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("hola juego de cartas")
+let divCartas = ""
+const arrayCartas = [
+  {
+    id: 1,
+    created_at: "2026-04-21 10:01:17.276873+00",
+    nombre: "Espíritu de hielo",
+    elixir: "1",
+    imagen: "https://static.wikia.nocookie.net/clash-royale-esp/images/9/96/Espiritu_de_Hielo.png/revision/latest?cb=20160705173003&path-prefix=es"
+  },
+  {
+    id: 2,
+    created_at: "2026-04-21 10:01:47.752481+00",
+    nombre: "Esqueletos",
+    elixir: "2",
+    imagen: "https://static.wikia.nocookie.net/clash-royale-esp/images/d/de/Esqueletos2.webp/revision/latest?cb=20231001211615&path-prefix=es"
+  },
+  {
+    id: 3,
+    created_at: "2026-04-21 10:02:53.582416+00",
+    nombre: "Montapuercos",
+    elixir: "4",
+    imagen: "https://static.wikia.nocookie.net/clash-royale-esp/images/4/45/Hog_Rider.png/revision/latest?cb=20170316084826&path-prefix=es"
+  }
+]
+
+console.log(arrayCartas[0])
